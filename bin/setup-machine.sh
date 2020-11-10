b76891016d5f657326b4baa5aa815a79a0bfad2d
John Faust <45057319+slickag@users.noreply.github.com>
2020-11-10 02:10:40 -0700

    Letting apt update gh, ripgrep, and jc.

--- bin/setup-machine.sh
+++ bin/setup-machine.sh
@@ -390,10 +390,10 @@ add_to_sudoers
 install_packages
 install_b2
 install_vscode
-install_ripgrep
-install_jc
+#install_ripgrep
+#install_jc
 install_bat
-install_gh
+#install_gh
 install_exa
 # install_fonts
 
